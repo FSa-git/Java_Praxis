36de0a137b665186e033575491989e98011c3092
F.Sa <114875448+FSa-git@users.noreply.github.com>
2022-11-21 19:49:35 +0100

    Add files via upload

---
+++ Notenvergeber.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class Notenvergeber {
+
+	public static void main(String[] args) {
+		
+		String sName [] = {"Peter", "Claus", "Ulli", "Magdalena"};
+        int iNoten[] = {6,5,2,1,3,4};
+
+        System.out.println("Hallo " + sName[2] + "! ");
+        
+        System.out.println("Du hast die Note: " + iNoten[2] + " bekommen!");
+		
+
+	}
+
+}
