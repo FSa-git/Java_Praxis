3768d888dc7c764659d3b8f9dc47a4985aafce87
F.Sa <114875448+FSa-git@users.noreply.github.com>
2022-11-21 19:51:33 +0100

    Add files via upload
    Zombie Array

---
+++ ar_1.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class ar_1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome our Zombie Killer programm!!!");
+		
+		String backback[] = {"Shotgun","Rifle","Sniper"};
+		String zombies[] = {"Peter","Mid range-zombie","Franci"};
+		int zahlen[] = {4,90,88,192};
+		
+		System.out.println("Backpack Items!");
+		System.out.println(backback[0]);
+		System.out.println(backback[1]);
+		System.out.println(backback[2]);
+		
+		System.out.println(zombies[2]);
+		System.out.println(zahlen[3]);
+
+	}
+
+}
